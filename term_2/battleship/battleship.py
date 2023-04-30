225a4f74d08004c71573397dd62c12c3a5997483
Siddharth <st22041@ormiston.school.nz>
2023-05-01 11:01:47 +1200

    Initial commit

--- term_2/battleship/battleship.py
+++ term_2/battleship/battleship.py
@@ -45,48 +45,51 @@ comp_ships_board = [
     ["0", "0", "0", "0", "0", "0", "0", "0"],
 ]
 
+
 def assign_ship(ships_board):
     SHIP_LENGTH = 3
 
     is_vertical = False
     is_horizontal = False
 
-    if randint(1,2) == 1:
+    if randint(1, 2) == 1:
         is_vertical = True
     else:
         is_horizontal = True
-    
+
     if is_horizontal == True:
-        selected_row = randint(0,7)
-        selected_col = randint(0,5)
+        selected_row = randint(0, 7)
+        selected_col = randint(0, 5)
         for i in range(SHIP_LENGTH):
-            ships_board[selected_row][selected_col] = '1'
+            ships_board[selected_row][selected_col] = "1"
             selected_col += 1
     elif is_vertical == True:
-        selected_row = randint(0,5)
-        selected_col = randint(0,7)
+        selected_row = randint(0, 5)
+        selected_col = randint(0, 7)
         for i in range(SHIP_LENGTH):
-            ships_board[selected_row][selected_col] = '1'
+            ships_board[selected_row][selected_col] = "1"
             selected_row += 1
     print(ships_board)
+
 
 def player_check_hit():
     col = int(input("Enter x coordinate 1-8: ")) - 1
     row = int(input("Enter y coordinate 1-8: ")) - 1
 
-    if comp_ships_board[row][col] == '1':
+    if comp_ships_board[row][col] == "1":
         print("Player hit")
         player_hit_board[row][col] = "X"
-    elif comp_ships_board[row][col] == '0':
+    elif comp_ships_board[row][col] == "0":
         print("Player missed")
         player_hit_board[row][col] = "*"
     else:
         print("You have already targeted this coordinate!")
 
+
 def comp_turn():
     time.sleep(0.2)
-    row = randint(0,7)
-    col = randint(0,7)
+    row = randint(0, 7)
+    col = randint(0, 7)
 
     if player_ships_board[row][col] == "0":
         print("Computer missed")
@@ -97,16 +100,17 @@ def comp_turn():
     else:
         comp_turn()
 
+
 def check_win_condition():
     hit_var = 0
     other_hit_var = 0
     for i in player_hit_board:
         for j in i:
-            if j == 'X':
+            if j == "X":
                 hit_var += 1
     for i in player_ships_board:
         for j in i:
-            if j == 'X':
+            if j == "X":
                 other_hit_var += 1
     if hit_var == 3 and other_hit_var == 3:
         quit("Stalemate. Both ships sunk!")
@@ -114,6 +118,7 @@ def check_win_condition():
         quit("Victory. You sunk the enemy battleship!")
     if other_hit_var == 3:
         quit("Defeat. Your battleship was sunk!")
+
 
 assign_ship(player_ships_board)
 assign_ship(comp_ships_board)
@@ -128,5 +133,5 @@ while True:
     player_check_hit()
 
     comp_turn()
-    
+
     check_win_condition()
